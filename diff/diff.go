709d17dfeb5a34e3debdde8e7a0fd47e26c64953
rzcTree <rluckzflyc@gmail.com>
2017-12-09 02:51:02 +0800

    20171209

--- diff/diff.go
+++ diff/diff.go
@@ -6,16 +6,26 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"sync"
+	// "sync"
 )
 
 var srcFile = "SrcFile"
 var dstFile = "DstFile"
 var lg *log.Logger
-var wg sync.WaitGroup
+
+// var wg sync.WaitGroup
+
+var srcBytes [][]byte
+var srcBLen int
+var dstBytes [][]byte
+var dstBLen int
 
 func init() {
 	lg = log.New(os.Stdout, "diff ", log.Lshortfile)
+	srcBytes = readFile(srcFile)
+	srcBLen = len(srcBytes)
+	dstBytes = readFile(dstFile)
+	dstBLen = len(dstBytes)
 }
 
 // src X, dst Y
@@ -24,28 +34,75 @@ type point struct {
 	y         int
 	children  []*point
 	ready     chan struct{}
-	bestChild int
+	bestChild *point
 	distance  int
+}
+
+func (p *point) getBestPath() {
+	// fmt.Println()
+	// fmt.Println(p)
+	if p.x == srcBLen && p.y == dstBLen {
+		p.distance = 0
+		close(p.ready)
+		return
+		// return p
+	}
+	childNum := len(p.children)
+	if childNum == 0 {
+		lg.Fatalf("Point (x:%d,y:%d) childern number is 0, please check", p.x, p.y)
+	}
+	// fmt.Println(p.children)
+	p.bestChild = minDistance(p.children)
+	p.distance = p.bestChild.distance + 1
+	close(p.ready)
+	// return nil
+}
+
+func minDistance(pts []*point) *point {
+	// fmt.Println(len(pts))
+	var min *point
+	// fmt.Println("1")
+	if _, ok := <-pts[0].ready; !ok {
+		// fmt.Println("in")
+		min = pts[0]
+		// fmt.Println(min)
+	}
+	// fmt.Println("2")
+	for i, v := range pts {
+		if i == 0 {
+			continue
+		}
+		if _, ok := <-v.ready; !ok {
+			// fmt.Println("ok")
+			// fmt.Printf("%d,%v,%v\n", i, v, min)
+			// fmt.Printf("%d,%d\n", v.distance, min.distance)
+			if v.distance < min.distance {
+				min = v
+			}
+		}
+	}
+	// fmt.Println(min)
+	return min
 }
 
 func (p *point) String() string {
 	var s string
-	for _, v := range p.children {
-		s += fmt.Sprintf(" %d,%d", v.x, v.y)
-	}
+	// for _, v := range p.children {
+	// 	s += fmt.Sprintf(" %d,%d", v.x, v.y)
+	// }
 	return fmt.Sprintf("%d,%d", p.x, p.y) + "\t" + s
 }
 
 func initPoint(x int, y int) *point {
 	p := new(point)
 	children := make([]*point, 0, 3)
-	bestChild := int(-1)
+	// bestChild := int(-1)
 	ready := make(chan struct{})
 	p.x = x
 	p.y = y
 	p.children = children
 	p.ready = ready
-	p.bestChild = bestChild
+	p.bestChild = nil
 	p.distance = int(-1)
 	return p
 }
@@ -60,11 +117,7 @@ func readFile(file string) [][]byte {
 	return fileBytes
 }
 
-func InitGraph(src string, dst string) [][]*point {
-	srcBytes := readFile(src)
-	srcBLen := len(srcBytes)
-	dstBytes := readFile(dst)
-	dstBLen := len(dstBytes)
+func InitGraph() [][]*point {
 	theSame := make(map[int][]int)
 	graph := make([][]*point, 0, (srcBLen + 1))
 	for i := 0; i <= srcBLen; i++ {
@@ -98,6 +151,7 @@ func InitGraph(src string, dst string) [][]*point {
 				for _, vv := range v {
 					if j == vv {
 						graph[i][j].children = append(graph[i][j].children, graph[i+1][j+1])
+						// fmt.Println(graph[i+1][j+1])
 						break
 					}
 				}
@@ -108,5 +162,24 @@ func InitGraph(src string, dst string) [][]*point {
 }
 
 func Diff() {
-	fmt.Println("Test.")
+	// fmt.Println("Test.")
+	graph := InitGraph()
+	for _, vx := range graph {
+		for _, vy := range vx {
+			// fmt.Println(vy)
+			go vy.getBestPath()
+		}
+	}
+	if _, ok := <-graph[0][0].ready; !ok {
+		printPoint(graph[0][0])
+	}
+}
+
+func printPoint(p *point) {
+	fmt.Println(p)
+	if p.bestChild == nil {
+		return
+	} else {
+		printPoint(p.bestChild)
+	}
 }
--- main.go
+++ main.go
@@ -1,21 +1,22 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"github.com/jacenr/filediff/diff"
 )
 
 func main() {
-	// diff.Diff()
-	var srcFile = "SrcFile"
-	var dstFile = "DstFile"
+	diff.Diff()
+	// var srcFile = "SrcFile"
+	// var dstFile = "DstFile"
 	// a := diff.ReadFile(srcFile)
 	// fmt.Println(a)
-	graph := diff.InitGraph(srcFile, dstFile)
-	// fmt.Println()
-	for _, val := range graph {
-		for _, val2 := range val {
-			fmt.Println(val2)
-		}
-	}
+	// graph := diff.InitGraph()
+	// // fmt.Println()
+	// for i, val := range graph {
+	// 	for j, val2 := range val {
+	// 		// fmt.Println(val2)
+	// 		fmt.Printf("%d,%d\t%v\n", i, j, val2)
+	// 	}
+	// }
 }
