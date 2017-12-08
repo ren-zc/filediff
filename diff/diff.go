dc62c80aaa506a48c36dc2bdfadb378ce49458e4
rzcTree <rluckzflyc@gmail.com>
2017-12-08 10:14:27 +0800

    20171208

---
+++ diff/diff.go
@@ -0,0 +1,9 @@
+package diff
+
+import (
+	"fmt"
+)
+
+func Diff() {
+	fmt.Println("Test.")
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	// "fmt"
+	"github.com/jacenr/filediff/diff"
+)
+
+func main() {
+	diff.Diff()
+}
