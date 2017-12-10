397d29714924a22281aa864359f0ddd45edd7f97
rzcTree <rluckzflyc@gmail.com>
2017-12-10 12:29:54 +0800

    20171210

--- diff/diff.go
+++ diff/diff.go
@@ -8,22 +8,14 @@ import (
 	"os"
 )
 
-var srcFile = "SrcFile"
-var dstFile = "DstFile"
+var srcFile string
+var dstFile string
 var lg *log.Logger
 
 var srcBytes [][]byte
 var srcBLen int
 var dstBytes [][]byte
 var dstBLen int
-
-func init() {
-	lg = log.New(os.Stdout, "diff ", log.Lshortfile)
-	srcBytes = readFile(srcFile)
-	srcBLen = len(srcBytes)
-	dstBytes = readFile(dstFile)
-	dstBLen = len(dstBytes)
-}
 
 // src X, dst Y
 type point struct {
@@ -33,6 +25,68 @@ type point struct {
 	ready     chan struct{}
 	bestChild *point
 	distance  int
+}
+
+func (p *point) String() string {
+	var s string
+	for _, v := range p.children {
+		s += fmt.Sprintf(" %d,%d", v.x, v.y)
+	}
+	return fmt.Sprintf("%d,%d", p.x, p.y) + "\t" + s
+}
+
+func initPoint(x int, y int) *point {
+	p := new(point)
+	children := make([]*point, 0, 3)
+	ready := make(chan struct{})
+	p.x = x
+	p.y = y
+	p.children = children
+	p.ready = ready
+	p.bestChild = nil
+	p.distance = int(-1)
+	return p
+}
+
+func readFile(file string) ([][]byte, error) {
+	fileContent, RErr := ioutil.ReadFile(file)
+	if RErr != nil {
+		return nil, RErr
+	}
+	fileBytes := bytes.Split(fileContent, []byte("\r\n")) // windows
+	// fileBytes := bytes.Split(fileContent, []byte{'\n'})   // linux
+	return fileBytes, nil
+}
+
+func initData() error {
+	lg = log.New(os.Stdout, "diff ", log.Lshortfile)
+	var readErr error
+	srcBytes, readErr = readFile(srcFile)
+	if readErr != nil {
+		return readErr
+	}
+	srcBLen = len(srcBytes)
+	dstBytes, readErr = readFile(dstFile)
+	if readErr != nil {
+		return readErr
+	}
+	dstBLen = len(dstBytes)
+	return nil
+}
+
+func minDistance(pts []*point) *point {
+	var min *point
+	if _, ok := <-pts[0].ready; !ok {
+		min = pts[0]
+	}
+	for _, v := range pts[1:] {
+		if _, ok := <-v.ready; !ok {
+			if v.distance < min.distance {
+				min = v
+			}
+		}
+	}
+	return min
 }
 
 func (p *point) getBestPath() {
@@ -50,57 +104,7 @@ func (p *point) getBestPath() {
 	close(p.ready)
 }
 
-func minDistance(pts []*point) *point {
-	var min *point
-	if _, ok := <-pts[0].ready; !ok {
-		min = pts[0]
-	}
-	for _, v := range pts[1:] {
-		// if i == 0 {
-		// 	continue
-		// }
-		if _, ok := <-v.ready; !ok {
-			if v.distance < min.distance {
-				min = v
-			}
-		}
-	}
-	return min
-}
-
-func (p *point) String() string {
-	var s string
-	// for _, v := range p.children {
-	// 	s += fmt.Sprintf(" %d,%d", v.x, v.y)
-	// }
-	return fmt.Sprintf("%d,%d", p.x, p.y) + "\t" + s
-}
-
-func initPoint(x int, y int) *point {
-	p := new(point)
-	children := make([]*point, 0, 3)
-	// bestChild := int(-1)
-	ready := make(chan struct{})
-	p.x = x
-	p.y = y
-	p.children = children
-	p.ready = ready
-	p.bestChild = nil
-	p.distance = int(-1)
-	return p
-}
-
-func readFile(file string) [][]byte {
-	fileContent, RErr := ioutil.ReadFile(file)
-	if RErr != nil {
-		lg.Fatalln(RErr)
-	}
-	fileBytes := bytes.Split(fileContent, []byte("\r\n")) // windows
-	// fileBytes := bytes.Split(fileContent, []byte{'\n'})   // linux
-	return fileBytes
-}
-
-func InitGraph() [][]*point {
+func initGraph() [][]*point {
 	theSame := make(map[int][]int)
 	graph := make([][]*point, 0, (srcBLen + 1))
 	for i := 0; i <= srcBLen; i++ {
@@ -143,27 +147,33 @@ func InitGraph() [][]*point {
 	return graph
 }
 
-func Diff() {
+func Diff(dst string, src string) ([][]byte, error) {
+	dstFile = dst
+	srcFile = src
+	initErr := initData()
+	if initErr != nil {
+		return nil, initErr
+	}
 	result := make([][]byte, 0, (srcBLen + dstBLen + 1))
-	result = append(result, []byte("@@@ S: src, D: dst. @@@"))
-	graph := InitGraph()
+	// result = append(result, []byte("@@@ S: src, D: dst. @@@"))
+	graph := initGraph()
 	for _, pl := range graph {
 		for _, p := range pl {
 			go p.getBestPath()
 		}
 	}
 	pList := make([]*point, 0, (srcBLen + dstBLen + 1))
-	var printPoint func(p *point)
-	printPoint = func(p *point) {
+	var travelPoint func(p *point)
+	travelPoint = func(p *point) {
 		pList = append(pList, p)
 		if p.bestChild == nil {
 			return
 		} else {
-			printPoint(p.bestChild)
+			travelPoint(p.bestChild)
 		}
 	}
 	if _, ok := <-graph[0][0].ready; !ok {
-		printPoint(graph[0][0])
+		travelPoint(graph[0][0])
 	}
 	growResult := func(sr string, i int, byteList [][]byte) {
 		s := fmt.Sprintf("%s %d ", sr, i+1)
@@ -178,19 +188,17 @@ func Diff() {
 		dx := p.x - tmp.x
 		dy := p.y - tmp.y
 		if dy == 0 {
-			growResult("- S", tmp.x, srcBytes)
+			growResult("- ", tmp.x, srcBytes)
 			tmp = p
 			continue
 		}
 		if dx == 0 {
-			growResult("+ D", tmp.y, dstBytes)
+			growResult("+ ", tmp.y, dstBytes)
 			tmp = p
 			continue
 		}
-		growResult("  D", tmp.y, dstBytes)
+		growResult("  ", tmp.y, dstBytes)
 		tmp = p
 	}
-	for _, byList := range result {
-		fmt.Printf("%s\n", byList)
-	}
+	return result, nil
 }
